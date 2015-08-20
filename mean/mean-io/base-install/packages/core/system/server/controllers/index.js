3309937431a419ba6aeac4e8276883a13a117a78
Pedro Mass <drom296@gmail.com>
2015-08-19 21:01:53 -0400

    Started looking into mean.io again

---
+++ mean/mean-io/base-install/packages/core/system/server/controllers/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var mean = require('meanio');
+module.exports = function(System){
+  return {
+    render:function(req,res){
+      res.render('index',{ locals: { config: System.config.clean }});
+    }
+  };
+}
