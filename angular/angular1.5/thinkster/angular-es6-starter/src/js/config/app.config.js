5741c0c11e262971cee0267b3fb595e660139dfd
Pedro Mass <pedro.j.mass@gmail.com>
2016-05-03 15:38:21 -0400

    Finished 'persisting JWT auth'

--- angular/angular1.5/thinkster/angular-es6-starter/src/js/config/app.config.js
+++ angular/angular1.5/thinkster/angular-es6-starter/src/js/config/app.config.js
@@ -1,5 +1,10 @@
+import authInterceptor from './auth.interceptor';
+
 function AppConfig($httpProvider, $stateProvider, $locationProvider, $urlRouterProvider) {
   'ngInject';
+
+  // Push our interceptor for auth
+  $httpProvider.interceptors.push(authInterceptor);
 
   /*
     If you don't want hashbang routing, uncomment this line.
@@ -8,10 +13,15 @@ function AppConfig($httpProvider, $stateProvider, $locationProvider, $urlRouterP
   // $locationProvider.html5Mode(true);
 
   $stateProvider
-  .state('app', {
-    abstract: true,
-    templateUrl: 'layout/app-view.html'
-  });
+    .state('app', {
+      abstract: true,
+      templateUrl: 'layout/app-view.html',
+      resolve: {
+        auth: function(User) {
+          return User.verifyAuth();
+        }
+      }
+    });
 
   $urlRouterProvider.otherwise('/');
 
--- angular/angular1.5/thinkster/angular-es6-starter/src/js/services/user.service.js
+++ angular/angular1.5/thinkster/angular-es6-starter/src/js/services/user.service.js
@@ -1,11 +1,12 @@
 export default class User {
-  constructor(JWT, AppConstants, $http, $state) {
+  constructor(JWT, AppConstants, $http, $state, $q) {
     'ngInject';
 
     this._JWT = JWT;
     this._AppConstants = AppConstants;
     this._$http = $http;
     this._$state = $state;
+    this._$q = $q;
 
     // Object to store our user properties
     this.current = null;
@@ -41,4 +42,59 @@ export default class User {
     // Do a hard reload of current state to ensure all data is flushed
     this._$state.go(this._$state.$current, null, { reload: true });
   }
+
+  verifyAuth() {
+    let deferred = this._$q.defer();
+
+    // check for JWT token first
+    if (!this._JWT.get()) {
+      deferred.resolve(false);
+      return deferred.promise;
+    }
+
+    // if there's a JWT & user is already Set
+    if (this.current) {
+      deferred.resolve(true);
+
+      // If current user isn't set, get it from the server
+      // If server doesn't 401, set current user & resolve promise.
+    } else {
+        this._$http({
+          url: this._AppConstants.api + '/user',
+          method: 'GET'
+        }).then(
+          (res) => {
+            this.current = res.data.user;
+            deferred.resolve(true);
+          },
+          // if an error happens, that means the user's token was invalid
+          (err) => {
+            this._JWT.destroy();
+            deferred.resolve(false);
+          }
+          // Reject automatically handled by auth interceptor
+          // Will boot them to homepage
+        );
+    }
+
+    return deferred.promise;
+  }
+
+  // This method will be used by UI-Router resolves
+  ensureAuthIs(bool) {
+    let deferred = this._$q.defer();
+
+    this.verifyAuth().then((authValid) => {
+      //if it's the opposite, redirect home
+      if (authValid !== bool) {
+        this._$state.go('app.home');
+        deferred.resolve(false);
+      } else {
+        deferred.resolve(true);
+      }
+    });
+
+    return deferred.promise;
+  }
+
 }
