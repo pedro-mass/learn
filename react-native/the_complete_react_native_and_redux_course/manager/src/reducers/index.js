1a470048dede713388aad369f6cc8a381bbbeec1
Pedro Mass <pedro.j.mass@gmail.com>
2017-04-17 10:13:29 -0400

    Finished lecture 148 - list building in employee list

--- react-native/the_complete_react_native_and_redux_course/manager/src/reducers/index.js
+++ react-native/the_complete_react_native_and_redux_course/manager/src/reducers/index.js
@@ -1,10 +1,10 @@
 import { combineReducers } from 'redux';
 import AuthReducer from './AuthReducer';
 import EmployeeFormReducer from './EmployeeFormReducer';
-import EmployeeReducer from './EmployeeReducer';
+import EmployeesReducer from './EmployeesReducer';
 
 export default combineReducers({
   auth: AuthReducer,
   employeeForm: EmployeeFormReducer,
-  employee: EmployeeReducer
+  employees: EmployeesReducer
 });
