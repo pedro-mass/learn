76fdec49527d1f3f09cf7555b60fff46518bbd9f
Pedro Mass <pedro.j.mass@gmail.com>
2017-04-25 20:03:27 -0400

    Finished lecture 159 - styling the modal

--- react-native/the_complete_react_native_and_redux_course/manager/src/components/EmployeeEdit.js
+++ react-native/the_complete_react_native_and_redux_course/manager/src/components/EmployeeEdit.js
@@ -1,12 +1,15 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Card, CardSection, Button } from './common';
+import Communications from 'react-native-communications';
+import { Card, CardSection, Button, Confirm } from './common';
 import EmployeeForm from './EmployeeForm';
 import { employeeUpdate, employeeSave } from '../actions';
 
 
 class EmployeeEdit extends Component {
+  state = { showModal: false };
+
   componentWillMount() {
     _.each(this.props.employee, (value, prop) => {
       this.props.employeeUpdate({ prop, value });
@@ -21,6 +24,18 @@ class EmployeeEdit extends Component {
     this.props.employeeSave({ name, phone, shift, uid: this.props.employee.uid });
   }
 
+  onTextPress() {
+    const { phone, shift } = this.props;
+
+    Communications.text(phone, `Your upcoming shift is on ${shift}`);
+  }
+
+  onFirePress() {
+    this.setState({
+      showModal: !this.state.showModal
+    });
+  }
+
   render() {
     return (
       <Card>
@@ -31,6 +46,24 @@ class EmployeeEdit extends Component {
             Save Changes
           </Button>
         </CardSection>
+
+        <CardSection>
+          <Button onPress={this.onTextPress.bind(this)}>
+            Text Schedule
+          </Button>
+        </CardSection>
+
+        <CardSection>
+          <Button onPress={this.onFirePress.bind(this)}>
+            Fire Employee
+          </Button>
+        </CardSection>
+
+        <Confirm
+          visible={this.state.showModal}
+        >
+          Are you sure you want to delete this?
+        </Confirm>
       </Card>
     );
   }
