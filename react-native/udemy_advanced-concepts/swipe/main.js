9e757f6d19f0461a00999f32def1f6706e5999c1
Pedro Mass <pedro.j.mass@gmail.com>
2017-06-08 09:59:13 -0400

    Got the react-native-elements working too

--- react-native/udemy_advanced-concepts/swipe/main.js
+++ react-native/udemy_advanced-concepts/swipe/main.js
@@ -1,7 +1,7 @@
 import Expo from 'expo';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-// import { Card, Button } from 'react-native-elements';
+import { Card, Button } from 'react-native-elements';
 import Deck from './src/Deck';
 
 const DATA = [
@@ -18,21 +18,19 @@ const DATA = [
 class App extends React.Component {
   renderCard(item) {
     return (
-      <Text>{item.text}</Text>
-
-      // <Card
-      //   title={item.text}
-      //   image={{ uri: item.uri }}
-      // >
-      //   <Text style={{ marginBottom: 10 }}>
-      //     I can customize the Card further
-      //   </Text>
-      //   <Button
-      //     icon={{ name: 'code' }}
-      //     backgroundColor="#03A9F4"
-      //     title="View Now!"
-      //   />
-      // </Card>
+      <Card
+        title={item.text}
+        image={{ uri: item.uri }}
+      >
+        <Text style={{ marginBottom: 10 }}>
+          I can customize the Card further
+        </Text>
+        <Button
+          icon={{ name: 'code' }}
+          backgroundColor="#03A9F4"
+          title="View Now!"
+        />
+      </Card>
     );
   }
 
