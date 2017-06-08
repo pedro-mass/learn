ebb25d911dfdc1cbb90fcba1f66b50d63bdf8355
Pedro Mass <pedro.j.mass@gmail.com>
2017-06-08 09:10:13 -0400

    Added a basic animation

--- react-native/udemy_advanced-concepts/swipe/main.js
+++ react-native/udemy_advanced-concepts/swipe/main.js
@@ -1,12 +1,13 @@
 import Expo from 'expo';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Ball from './src/Ball';
 
 class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up main.js to start working on your app!</Text>
+        <Ball />
       </View>
     );
   }
@@ -15,9 +16,7 @@ class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#fff'
   },
 });
 
