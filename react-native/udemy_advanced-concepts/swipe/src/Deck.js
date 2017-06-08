a243c49713dfd94728c708a6464c0e8db85654fe
Pedro Mass <pedro.j.mass@gmail.com>
2017-06-08 15:00:59 -0400

    Finished lecture 18

--- react-native/udemy_advanced-concepts/swipe/src/Deck.js
+++ react-native/udemy_advanced-concepts/swipe/src/Deck.js
@@ -1,7 +1,21 @@
 import React, { Component } from 'react';
-import { View, Animated } from 'react-native';
+import { View, Animated, PanResponder } from 'react-native';
 
 class Deck extends Component {
+  constructor(props) {
+    super(props);
+
+    const panResponder = PanResponder.create({
+      onStartShouldSetPanResponder: () => {},
+      onPanResponderMove: (event, gesture) => {
+        console.log('gesture', gesture);
+      },
+      onPanResponderRelease: () => {}
+    });
+
+    this.state = { panResponder };
+  }
+
   renderCards() {
     return this.props.data.map(item => {
       return this.props.renderCard(item);
