bdb0bb46f2349fff6fa42adc9eeb44f1c81887e1
Pedro Mass <pedro.j.mass@gmail.com>
2017-06-09 08:48:22 -0400

    lecture 29: forcing swipes

--- react-native/udemy_advanced-concepts/swipe/src/Deck.js
+++ react-native/udemy_advanced-concepts/swipe/src/Deck.js
@@ -3,6 +3,7 @@ import { View, Animated, PanResponder, Dimensions } from 'react-native';
 
 const SCREEN_WIDTH = Dimensions.get('window').width;
 const SWIPE_THRESHOLD = 0.25 * SCREEN_WIDTH;
+const SWIPEOUT_DURATION = 250;
 
 class Deck extends Component {
   constructor(props) {
@@ -17,9 +18,9 @@ class Deck extends Component {
       },
       onPanResponderRelease: (event, gesture) => {
         if (gesture.dx > SWIPE_THRESHOLD) {
-          console.log('swipe right!');
+          this.forceSwipe('right');
         } else if (gesture.dx < -SWIPE_THRESHOLD) {
-          console.log('swipe left')
+          this.forceSwipe('left');
         } else {
           this.resetPosition();
         }
@@ -28,6 +29,15 @@ class Deck extends Component {
 
     this.position = position;
     this.panResponder = panResponder;
+  }
+
+  forceSwipe(direction) {
+    const x = direction === 'right' ? SCREEN_WIDTH : -SCREEN_WIDTH;
+
+    Animated.timing(this.position, {
+      toValue: { x, y: 0 },
+      duration: SWIPEOUT_DURATION
+    }).start();
   }
 
   resetPosition() {
