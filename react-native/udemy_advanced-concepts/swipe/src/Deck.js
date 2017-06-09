de031556e93483c9a442af3b1af018d871c498ea
Pedro Mass <pedro.j.mass@gmail.com>
2017-06-09 09:15:01 -0400

    lecture 35: handling empty lists

--- react-native/udemy_advanced-concepts/swipe/src/Deck.js
+++ react-native/udemy_advanced-concepts/swipe/src/Deck.js
@@ -78,6 +78,10 @@ class Deck extends Component {
   }
 
   renderCards() {
+    if (this.state.index >= this.props.data.length) {
+      return this.props.renderNoMoreCards();
+    }
+
     return this.props.data.map((item, i) => {
       if (i < this.state.index) { return null; }
 
