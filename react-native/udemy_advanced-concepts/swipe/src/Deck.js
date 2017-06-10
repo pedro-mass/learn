420231077b26c5545fd0741061f05d0155c68d4f
Pedro Mass <pedro.j.mass@gmail.com>
2017-06-10 14:57:52 -0400

    lecture 39: animating advancing cards

--- react-native/udemy_advanced-concepts/swipe/src/Deck.js
+++ react-native/udemy_advanced-concepts/swipe/src/Deck.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Animated, PanResponder, Dimensions } from 'react-native';
+import { View, Animated, PanResponder, Dimensions, LayoutAnimation, UIManager } from 'react-native';
 
 const SCREEN_WIDTH = Dimensions.get('window').width;
 const SWIPE_THRESHOLD = 0.25 * SCREEN_WIDTH;
@@ -39,6 +39,11 @@ class Deck extends Component {
     this.state = {
       index: 0
     };
+  }
+
+  componentWillUpdate() {
+    UIManager.setLayoutAnimationEnabledExperimental && UIManager.setLayoutAnimationEnabledExperimental(true);
+    LayoutAnimation.spring();
   }
 
   forceSwipe(direction) {
