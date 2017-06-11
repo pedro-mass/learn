9f74c9ceaba85ba95d8123f359d46ecf784f3c23
Pedro Mass <pedro.j.mass@gmail.com>
2017-06-11 15:58:56 -0400

    lecture 52: generating a service account

--- react-native/udemy_advanced-concepts/one-time-password/functions/index.js
+++ react-native/udemy_advanced-concepts/one-time-password/functions/index.js
@@ -1,11 +1,12 @@
 const functions = require('firebase-functions');
 const createUser = require('./create_user');
+const admin = require("firebase-admin");
+const serviceAccount = require("./serviceAccountKey.json");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://one-time-password-15ad1.firebaseio.com"
 });
 
-export.createUser = functions.https.onRequest(createUser);
+
+exports.createUser = functions.https.onRequest(createUser);
