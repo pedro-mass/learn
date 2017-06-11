f6da2ed554d1e901b31725742c293f6e59de3bf4
Pedro Mass <pedro.j.mass@gmail.com>
2017-06-11 16:09:43 -0400

    lecture 53: sanitizing user inputs

--- react-native/udemy_advanced-concepts/one-time-password/functions/create_user.js
+++ react-native/udemy_advanced-concepts/one-time-password/functions/create_user.js
@@ -1,3 +1,17 @@
+const admin = require('firebase-admin');
+
 module.exports = function(req, res) {
-  res.send(req.body);
+  // verify the user provided a phone
+  if (!req.body.phone) {
+    return res.status(422).send({ error: 'Bad Input' });
+  }
+
+  // format the phone number to remove dashes and parens
+  // note: used the String constructor to handle the case of being passed in
+  // a number instead of a string
+  const phone = String(req.body.phone).replace(/[^\d]/g, '');
+
+  // create new user account using the phone
+
+  // respond to the user request, saying the account was made
 };
--- react-native/udemy_advanced-concepts/one-time-password/functions/index.js
+++ react-native/udemy_advanced-concepts/one-time-password/functions/index.js
@@ -3,6 +3,7 @@ const createUser = require('./create_user');
 const admin = require("firebase-admin");
 const serviceAccount = require("./serviceAccountKey.json");
 
+// configures the admin service account
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://one-time-password-15ad1.firebaseio.com"
