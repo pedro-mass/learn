1e48e98d36db0b3632cfc0addbf5cf797c6e3bd4
Pedro Mass <pedro.j.mass@gmail.com>
2017-06-15 20:01:43 -0400

    lecture 66: app boilerplate

--- react-native/udemy_advanced-concepts/firebase-auth/main.js
+++ react-native/udemy_advanced-concepts/firebase-auth/main.js
@@ -1,12 +1,13 @@
 import Expo from 'expo';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import SignUpForm from './components/SignUpForm';
 
 class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up main.js to start working on your app!</Text>
+        <SignUpForm />
       </View>
     );
   }
