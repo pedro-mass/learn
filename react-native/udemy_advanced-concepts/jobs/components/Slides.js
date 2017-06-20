537135d60aae640de21ed2773a017cd9a493d78b
Pedro Mass <pedro.j.mass@gmail.com>
2017-06-20 08:48:20 -0400

    lecture 90: the welcome screen

---
+++ react-native/udemy_advanced-concepts/jobs/components/Slides.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class Slides extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Slides</Text>
+        <Text>Slides</Text>
+        <Text>Slides</Text>
+        <Text>Slides</Text>
+      </View>
+    );
+  }
+}
+
+export default Slides;
