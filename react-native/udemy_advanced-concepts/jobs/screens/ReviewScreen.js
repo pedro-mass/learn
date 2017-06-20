aa1d2b234dfd4498bd57784808035894361838eb
Pedro Mass <pedro.j.mass@gmail.com>
2017-06-20 08:39:43 -0400

    lecture 89: styling the navBar

--- react-native/udemy_advanced-concepts/jobs/screens/ReviewScreen.js
+++ react-native/udemy_advanced-concepts/jobs/screens/ReviewScreen.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Platform } from 'react-native';
 import { Button } from 'react-native-elements'
 
 class ReviewScreen extends Component {
   static navigationOptions = ({ navigation }) => ({
     title: 'Review Jobs',
-    headerRight: <Button title="Settings" onPress={() => navigation.navigate('settings')}/>
+    headerRight: (<Button
+                    title="Settings"
+                    onPress={() => navigation.navigate('settings')}
+                    backgroundColor="rgba(0,0,0,0)"
+                    color="rgba(0, 122, 255, 1)"
+                  />),
+    headerStyle: {
+      marginTop: Platform.OS === 'android' ? Expo.Constants.statusBarHeight : undefined
+    }
   });
 
   render() {
