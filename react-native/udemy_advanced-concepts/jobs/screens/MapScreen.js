bd45bfb3e0a5a6326ac304bbad7f026030253248
Pedro Mass <pedro.j.mass@gmail.com>
2017-06-24 10:50:28 -0400

    lecture 110: interacting with the MapView

--- react-native/udemy_advanced-concepts/jobs/screens/MapScreen.js
+++ react-native/udemy_advanced-concepts/jobs/screens/MapScreen.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { MapView } from 'expo';
 
 class MapScreen extends Component {
+  state = {
+    region: {
+      longitude: -122,
+      latitude: 37,
+      longitudeDelta: 0.04,
+      latitudeDelta: 0.09
+    }
+  }
+
   render() {
     return (
-      <View>
-        <Text>MapScreen</Text>
-        <Text>MapScreen</Text>
-        <Text>MapScreen</Text>
-        <Text>MapScreen</Text>
+      <View style={{ flex: 1 }}>
+        <MapView style={{ flex: 1 }} region={this.state.region}/>
       </View>
     );
   }
