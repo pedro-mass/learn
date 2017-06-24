f3b66f03d5efe5eaaf929d4a65f000d231d6a81d
Pedro Mass <pedro.j.mass@gmail.com>
2017-06-24 11:59:17 -0400

    lecture 116: issuing action creators from mapScreen

--- react-native/udemy_advanced-concepts/jobs/actions/job_actions.js
+++ react-native/udemy_advanced-concepts/jobs/actions/job_actions.js
@@ -1,11 +1,13 @@
 import axios from 'axios';
 import reverseGeocode from 'latlng-to-zip';
 import secret from '../secret';
+import qs from 'qs';
 
 import {
   FETCH_JOBS
 } from './types';
 
+const JOB_ROOT_URL = 'http://api.indeed.com/ads/apisearch?';
 const JOB_QUERY_PARAMS = {
   publisher: secret.indeed.publisherId,
   format: 'json',
@@ -15,9 +17,20 @@ const JOB_QUERY_PARAMS = {
   q: 'javascript'
 };
 
+const buildJobsUrl = (zip) => {
+  const query = qs.stringify({ ...JOB_QUERY_PARAMS, l: zip });
+  return `${JOB_ROOT_URL}${query}`;
+};
+
 export const fetchJobs = (region) => async (dispatch) => {
   try {
     let zip = await reverseGeocode(region);
+    const url = buildJobsUrl(zip);
+
+    let { data } = await axios.get(url);
+
+    dispatch({ type: FETCH_JOBS, payload: data });
+    console.log(data);
   } catch(e) {
     console.error(e);
   }
