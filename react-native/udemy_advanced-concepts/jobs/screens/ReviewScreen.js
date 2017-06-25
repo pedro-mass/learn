38d493f29ce7e013e7941a3633b07528a4bd1d01
Pedro Mass <pedro.j.mass@gmail.com>
2017-06-25 16:21:15 -0400

    lecture 132 - mapping a jobs location

--- react-native/udemy_advanced-concepts/jobs/screens/ReviewScreen.js
+++ react-native/udemy_advanced-concepts/jobs/screens/ReviewScreen.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { View, Text, Platform, ScrollView, Linking } from 'react-native';
 import { Button, Card } from 'react-native-elements'
 import { connect } from 'react-redux';
+import { MapView } from 'expo';
 
 class ReviewScreen extends Component {
   static navigationOptions = ({ navigation }) => ({
@@ -18,9 +19,23 @@ class ReviewScreen extends Component {
 
   renderLikedJobs = () => {
     return this.props.likedJobs.map(job => {
+      const initialRegion = {
+        longitude: job.longitude,
+        latitude: job.latitude,
+        longitudeDelta: 0.045,
+        latitudeDelta: 0.02
+      };
+
       return (
         <Card>
           <View style={{ height: 200 }}>
+            <MapView
+              scrollEnabled={false}
+              style={{ flex: 1 }}
+              cacheEnabled={Platform.OS === 'android'}
+              initialRegion={initialRegion}
+            />
+
             <View style={styles.detailWrapper}>
               <Text style={styles.italics}>{job.company}</Text>
               <Text style={styles.italics}>{job.formattedRelativeTime}</Text>
