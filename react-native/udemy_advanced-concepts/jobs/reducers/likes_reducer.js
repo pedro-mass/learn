746ab09920a71bf061daab1bb00f9c3d157ec5f1
Pedro Mass <pedro.j.mass@gmail.com>
2017-06-27 07:14:16 -0400

    Added offline persistence

--- react-native/udemy_advanced-concepts/jobs/reducers/likes_reducer.js
+++ react-native/udemy_advanced-concepts/jobs/reducers/likes_reducer.js
@@ -1,8 +1,11 @@
 import _ from 'lodash';
+import { REHYDRATE } from 'redux-persist/constants';
 import { LIKE_JOB, CLEAR_LIKED_JOB } from '../actions/types';
 
 export default function(state = [], action) {
   switch (action.type) {
+    case REHYDRATE:
+      return action.payload.likedJobs || [];
     case CLEAR_LIKED_JOB:
       return [];
     case LIKE_JOB:
