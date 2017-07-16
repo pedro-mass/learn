c801b0e96d2a6c9ff2cf7acdeb493d273adce3e7
Pedro Mass <pedro.j.mass@gmail.com>
2017-07-16 12:25:57 -0400

    Finished section 2: ajax requests with React

--- react/udemy-modern_react_with_redux/ReduxSimpleStarter/src/index.js
+++ react/udemy-modern_react_with_redux/ReduxSimpleStarter/src/index.js
@@ -1,17 +1,51 @@
-import React from 'react';
+import _ from 'lodash';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import secret from './secret';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = secret.youtubeApiKey;
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+
+    this.videoSearch('surfboards');
+  }
+
+  videoSearch(term) {
+    YTSearch({ key: API_KEY, term}, (videos) => {
+      this.setState({
+        videos,
+        selectedVideo: videos[0]
+       });
+    });
+  }
+
+  render() {
+    const videoSearch = _.debounce(
+      (term) => { this.videoSearch(term) },
+      300
+    );
+
+    return (
+      <div>
+        <SearchBar onSearchTermChange={videoSearch}/>
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
+          videos={this.state.videos} />
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
