446e3d1207cee6b0336b8703625219e9157d5ad2
Pedro Mass <pedro.j.mass@gmail.com>
2017-07-17 21:10:47 -0400

    Displaying a list of subrreddits

---
+++ react/redux-step_by_step/src/components/ListView.js
@@ -0,0 +1,20 @@
+import _ from 'lodash';
+import React, { Component } from 'react';
+
+export default class ListView extends Component {
+  render() {
+    return (
+      <ul>
+        {_.map(this.props.rowsIdArray, this.renderRowById)}
+      </ul>
+    );
+  }
+
+  renderRowById = rowId => {
+    return (
+      <li key={rowId}>
+        {this.props.renderRow(_.get(this.props.rowsById, rowId))}
+      </li>
+    );
+  };
+}
