13b7d013906a5fa49d5a0bb799fdbc1a790bc1f0
Pedro Mass <pedro.j.mass@gmail.com>
2017-08-04 22:14:22 -0400

    Add GET /todos/:id

--- node/udemy-the_complete_nodejs_dev_course/node-todo-api/server/server.js
+++ node/udemy-the_complete_nodejs_dev_course/node-todo-api/server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
 
 const mongoose = require('./db/mongoose');
 const Todo = require('./models/Todo');
@@ -17,6 +18,26 @@ app.get('/todos', (req, res) => {
       res.send({ todos });
     })
     .catch(e => res.status(500).send(e));
+});
+
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id;
+
+  // validate id
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  // look for the todo
+  Todo.findById(id)
+    .then(todo => {
+      if (!todo) {
+        return res.status(404).send();
+      }
+
+      res.send({ todo });
+    })
+    .catch(e => res.status(400).send());
 });
 
 app.post('/todos', (req, res) => {
