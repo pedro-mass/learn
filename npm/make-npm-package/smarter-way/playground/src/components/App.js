0698668b51261aa0a4ab4efe91f68f3b31a329e6
Pedro Mass <pedro.j.mass@gmail.com>
2017-11-22 12:23:03 -0500

    Adds Label element and Button sizes

--- npm/make-npm-package/smarter-way/playground/src/components/App.js
+++ npm/make-npm-package/smarter-way/playground/src/components/App.js
@@ -1,15 +1,42 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import { Button } from "component-lib";
+import { Button, Label } from "component-lib";
 
 import "../assets/stylesheets/base.scss";
 
 const App = ({ name }) => {
   return (
     <div>
-      <h1>Hello, {name}!</h1>
-      <Button bgColor="orange">Click Me!</Button>
+      <Label size="small">Hello, {name}!</Label>
+      <div>
+        <Button bgColor="green" size="small">
+          small
+        </Button>
+      </div>
+      <div>
+        <Button bgColor="yellow" size="medium">
+          medium
+        </Button>
+      </div>
+      <div>
+        <Button bgColor="orange" size="large">
+          large
+        </Button>
+      </div>
+      <div>
+        <Button size="wide">wide</Button>
+      </div>
+      <div>
+        <Button bgColor="pink" size="extraWide">
+          extra wide
+        </Button>
+      </div>
+      <div>
+        <Button bgColor="purple" size="fullWidth">
+          full width
+        </Button>
+      </div>
     </div>
   );
 };
