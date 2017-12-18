698efd284a64413a3f353673b57c7546b5696b7c
Pedro Mass <pedro.j.mass@gmail.com>
2017-12-17 20:26:07 -0500

    Playing with react native again.

--- react-native/udemy_advanced-concepts/jobs/main.js
+++ react-native/udemy_advanced-concepts/jobs/main.js
@@ -1,63 +1,65 @@
-import Expo, { Notifications } from 'expo';
-import React from 'react';
-import { StyleSheet, Text, View, Alert } from 'react-native';
-import { TabNavigator, StackNavigator } from 'react-navigation';
-import { Provider } from 'react-redux';
+import Expo, { Notifications } from "expo";
+import React from "react";
+import { StyleSheet, Text, View, Alert } from "react-native";
+import { TabNavigator, StackNavigator } from "react-navigation";
+import { Provider } from "react-redux";
 
-import registerForNotifications from './services/push_notifications';
+import registerForNotifications from "./services/push_notifications";
 
-import store from './store';
-import AuthScreen from './screens/AuthScreen';
-import WelcomeScreen from './screens/WelcomeScreen';
-import MapScreen from './screens/MapScreen';
-import DeckScreen from './screens/DeckScreen';
-import SettingsScreen from './screens/SettingsScreen';
-import ReviewScreen from './screens/ReviewScreen';
+import store from "./store";
+import AuthScreen from "./screens/AuthScreen";
+import WelcomeScreen from "./screens/WelcomeScreen";
+import MapScreen from "./screens/MapScreen";
+import DeckScreen from "./screens/DeckScreen";
+import SettingsScreen from "./screens/SettingsScreen";
+import ReviewScreen from "./screens/ReviewScreen";
 
 class App extends React.Component {
   componentDidMount() {
     registerForNotifications();
 
-    Notifications.addListener((notifcation) => {
+    Notifications.addListener(notifcation => {
       const { data: { text }, origin } = notifcation;
 
-      if (origin === 'received' && text) {
-        Alert.alert(
-          'New Push Notification',
-          text,
-          [{ text: 'Ok' }]
-        );
+      if (origin === "received" && text) {
+        Alert.alert("New Push Notification", text, [{ text: "Ok" }]);
       }
     });
   }
 
   render() {
-    const MainNavigator = TabNavigator({
-      welcome: { screen: WelcomeScreen },
-      auth: { screen: AuthScreen },
-      main: {
-        screen: TabNavigator({
-          map: { screen: MapScreen },
-          deck: { screen: DeckScreen },
-          review: {
-            screen: StackNavigator({
-              review: { screen: ReviewScreen },
-              settings: { screen: SettingsScreen}
-            })
-          }
-        }, {
-          tabBarPosition: 'bottom', // used to make android place this bar in the bottom
-          tabBarOptions: {
-            labelStyle: { fontSize: 12 }
-          }
-        })
-      }
-    }, {
-      navigationOptions: {
-        tabBarVisible: false
+    const MainNavigator = TabNavigator(
+      {
+        welcome: { screen: WelcomeScreen },
+        auth: { screen: AuthScreen },
+        main: {
+          screen: TabNavigator(
+            {
+              map: { screen: MapScreen },
+              deck: { screen: DeckScreen },
+              review: {
+                screen: StackNavigator({
+                  review: { screen: ReviewScreen },
+                  settings: { screen: SettingsScreen }
+                })
+              }
+            },
+            {
+              tabBarPosition: "bottom", // used to make android place this bar in the bottom
+              tabBarOptions: {
+                labelStyle: { fontSize: 12 }
+              }
+            }
+          )
+        }
       },
-      lazy: true
-    });
+      {
+        navigationOptions: {
+          tabBarVisible: false
+        },
+        lazy: true
+      }
+    );
 
     return (
       <Provider store={store}>
@@ -72,9 +74,9 @@ class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    justifyContent: "center"
+  }
 });
 
 Expo.registerRootComponent(App);
