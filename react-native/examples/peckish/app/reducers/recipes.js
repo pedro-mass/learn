1c2df5e4dac3f26e89fc8bb0fd7f9309eaf62691
Pedro Mass <pedro.j.mass@gmail.com>
2018-03-06 22:00:15 -0500

    Updated peckish example

--- react-native/examples/peckish/app/reducers/recipes.js
+++ react-native/examples/peckish/app/reducers/recipes.js
@@ -1,15 +1,21 @@
-import createReducer from '../lib/createReducer'
-import * as types from '../actions/types'
+import createReducer from "../lib/createReducer";
+import * as types from "../actions/types";
 
-export const searchedRecipes = createReducer({}, {
-  [types.SET_SEARCHED_RECIPES](state, action) {
-    let newState = {}
-    action.recipes.forEach( (recipe) => {
-      let id = recipe.href
-      newState[id] = Object.assign({}, recipe, { id });
-    });
-    return newState;
-  },
+export const searchedRecipes = createReducer(
+  {},
+  {
+    ["SET_SEARCHED_RECIPES"](state, action) {
+      let newState = {};
+      action.recipes.forEach(recipe => {
+        let id = recipe.href;
+        newState[id] = Object.assign({}, recipe, { id });
+      });
+      return newState;
+    },
+    ["PEDRO"](state, action) {
+      const newState = {};
 
-});
-
+      return newState;
+    }
+  }
+);
