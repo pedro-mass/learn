2f809d14a8dda7e403ed1a0949fcc0a633a30568
Pedro Mass <pedro.j.mass@gmail.com>
2018-03-11 17:19:56 -0400

    Move diy-dev-server into it's own folder.

---
+++ webpack/udemy-webpack-course/diy-dev-server/config/webpack.dev.js
@@ -0,0 +1,79 @@
+const path = require("path");
+const webpack = require("webpack");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: {
+    main: ["./src/main.js"]
+  },
+  mode: "development",
+  output: {
+    filename: "[name]-bundle.js",
+    path: path.resolve(__dirname, "../dist"),
+    publicPath: "/"
+  },
+  devServer: {
+    contentBase: "dist",
+    port: "3000",
+    overlay: true,
+    hot: true
+  },
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: "babel-loader"
+          }
+        ],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            // injects css into html
+            loader: "style-loader"
+          },
+          {
+            // runs linting?
+            loader: "css-loader"
+          }
+        ]
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            // does the linting
+            loader: "html-loader",
+            options: {
+              attrs: ["img:src"]
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(jpg|gif|png)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "images/[name].[ext]"
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin(),
+    // does the same thing as extractLoader and fileLoader
+    new HTMLWebpackPlugin({
+      template: "./src/index.html"
+    })
+  ]
+};
---
+++ webpack/udemy-webpack-course/diy-dev-server/src/main.js
@@ -0,0 +1,4 @@
+require("babel-runtime/regenerator");
+require("webpack-hot-middleware/client?reload=true");
+require("./main.css");
+require("./index.html");
