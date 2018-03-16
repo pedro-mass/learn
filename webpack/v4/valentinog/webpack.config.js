93559e9ace13e6d1434810632805095780e14479
Pedro Mass <pedro.j.mass@gmail.com>
2018-03-15 20:23:03 -0400

    Setup html plugin.

--- webpack/v4/valentinog/webpack.config.js
+++ webpack/v4/valentinog/webpack.config.js
@@ -1,3 +1,5 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
   module: {
     rules: [
@@ -7,7 +9,22 @@ module.exports = {
         use: {
           loader: "babel-loader"
         }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: { minimize: true }
+          }
+        ]
       }
     ]
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html"
+      // filename: "./index.html"
+    })
+  ]
 };
