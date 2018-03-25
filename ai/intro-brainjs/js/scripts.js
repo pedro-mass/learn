5625cb589a2ceb9b1ad5b56381144aa85413d89d
Pedro Mass <pedro.j.mass@gmail.com>
2018-03-25 17:08:33 -0400

    Complete setup & data-handling

---
+++ ai/intro-brainjs/js/scripts.js
@@ -0,0 +1,33 @@
+let trainedNet;
+
+function encode(arg) {
+  return arg.split("").map(x => x.charCodeAt(0) / 255);
+}
+
+function processTrainingData(data) {
+  return data.map(d => {
+    return {
+      input: encode(d.input),
+      output: d.output
+    };
+  });
+}
+
+function train(data) {
+  let net = new brain.NeuralNetwork();
+  net.train(processTrainingData(data));
+  trainedNet = net.toFunction();
+  console.log("Finished training...");
+}
+
+function execute(input) {
+  let results = trainedNet(encode(input));
+  let output;
+  // results.trump > results.kardashian
+  //   ? (output = "Trump")
+  //   : (output = "Kardashian");
+  output = results.trump > results.kardashian ? "Trump" : "Kardashian";
+  return output;
+}
+
+train(trainingData);
