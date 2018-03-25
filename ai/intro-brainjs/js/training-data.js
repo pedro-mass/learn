5475bb1017708489eb7b08f4168be3a84b57aff1
Pedro Mass <pedro.j.mass@gmail.com>
2018-03-25 17:10:09 -0400

    Brought in training data

---
+++ ai/intro-brainjs/js/training-data.js
@@ -0,0 +1,30 @@
+const trainingData = [
+  {
+    input: "Inside Chi's nursery",
+    output: { kardashian: 1 }
+  },
+  {
+    input: "Why I dyed my hair pink",
+    output: { kardashian: 1 }
+  },
+  {
+    input:
+      "Feeling Blue (wearing @kkwbeauty powder contour in medium & dark contour kit as eye shadow, & a new lip coming soon)",
+    output: { kardashian: 1 }
+  },
+  {
+    input:
+      "I will be interviewed by @JudgeJeanine on @FoxNews at 9:00 P.M. Enjoy!",
+    output: { trump: 1 }
+  },
+  {
+    input:
+      "Dem Memo: FBI did not disclose who the clients were - the Clinton Campaign and the DNC. Wow!",
+    output: { trump: 1 }
+  },
+  {
+    input:
+      "Thank you to the great men and women of the United States @SecretService for a job well done!",
+    output: { trump: 1 }
+  }
+];
