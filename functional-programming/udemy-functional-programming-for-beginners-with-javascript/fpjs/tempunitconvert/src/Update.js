504204ca5e56fa3a31c3c6d01ae3c005519b3e97
Pedro Mass <pedro.j.mass@gmail.com>
2018-04-02 09:22:09 -0400

    Create update functions for updating model.

--- functional-programming/udemy-functional-programming-for-beginners-with-javascript/fpjs/tempunitconvert/src/Update.js
+++ functional-programming/udemy-functional-programming-for-beginners-with-javascript/fpjs/tempunitconvert/src/Update.js
@@ -1,6 +1,61 @@
-import * as R from 'ramda';
+import * as R from "ramda";
 
-function update (msg, model) {
+const MSGS = {
+  LEFT_VALUE_INPUT: "LEFT_VALUE_INPUT",
+  RIGHT_VALUE_INPUT: "RIGHT_VALUE_INPUT",
+  LEFT_UNIT_CHANGED: "LEFT_UNIT_SELECT",
+  RIGHT_UNIT_CHANGED: "RIGHT_UNIT_SELECT"
+};
+
+export function leftValueInputMsg(leftValue) {
+  return {
+    type: MSGS.LEFT_VALUE_INPUT,
+    leftValue
+  };
+}
+
+export function rightValueInputMsg(rightValue) {
+  return {
+    type: MSGS.RIGHT_VALUE_INPUT,
+    rightValue
+  };
+}
+
+export function leftUnitChangedMsg(leftUnit) {
+  return {
+    type: MSGS.LEFT_UNIT_CHANGED,
+    leftUnit
+  };
+}
+
+export function rightUnitChangedMsg(rightUnit) {
+  return {
+    type: MSGS.RIGHT_UNIT_CHANGED,
+    rightUnit
+  };
+}
+
+const toInt = R.pipe(parseInt, R.defaultTo(0));
+
+function update(msg, model) {
+  switch (msg.type) {
+    case MSGS.LEFT_VALUE_INPUT:
+      if (msg.leftValue === "")
+        return { ...model, sourceLeft: true, leftValue: "", rightValue: "" };
+      const leftValue = toInt(msg.leftValue);
+      return { ...model, sourceLeft: true, leftValue };
+    case MSGS.RIGHT_VALUE_INPUT:
+      if (msg.rightValue === "")
+        return { ...model, sourceLeft: true, leftValue: "", rightValue: "" };
+      const rightValue = toInt(msg.rightValue);
+      return { ...model, sourceLeft: false, rightValue };
+    case MSGS.LEFT_UNIT_CHANGED:
+      const { leftUnit } = msg;
+      return { ...model, leftUnit, sourceLeft: true };
+    case MSGS.RIGHT_UNIT_CHANGED:
+      const { rightUnit } = msg;
+      return { ...model, rightUnit, sourceLeft: false };
+  }
   return model;
 }
 
--- functional-programming/udemy-functional-programming-for-beginners-with-javascript/fpjs/tempunitconvert/src/View.js
+++ functional-programming/udemy-functional-programming-for-beginners-with-javascript/fpjs/tempunitconvert/src/View.js
@@ -4,6 +4,13 @@ import { h } from "virtual-dom";
 
 const { div, h1, pre, p, input, select, option } = hh(h);
 
+import {
+  leftValueInputMsg,
+  rightValueInputMsg,
+  leftUnitChangedMsg,
+  rightUnitChangedMsg
+} from "./Update";
+
 const comparisonView = div(
   { className: "fl w-third pa0 center tc" },
   p({ className: "pa0" }, "=")
@@ -18,15 +25,19 @@ function unitOptions(selectedUnit) {
   );
 }
 
-function unitSection(dispatch, unit, value) {
+function unitSection(dispatch, unit, value, inputMsg, unitMsg) {
   return div({ className: "w-50 ma1" }, [
     input({
       className: "db w-100 pa2 ba input-reset mv2",
       type: "text",
-      value
+      value,
+      oninput: e => dispatch(inputMsg(e.target.value))
     }),
     select(
-      { className: "db w-100 pa2 ba input-reset br1 bg-white ba b--black" },
+      {
+        className: "db w-100 pa2 ba input-reset br1 bg-white ba b--black",
+        onchange: e => dispatch(unitMsg(e.target.value))
+      },
       unitOptions(unit)
     )
   ]);
@@ -36,8 +47,20 @@ function view(dispatch, model) {
   return div({ className: "mw6 center" }, [
     h1({ className: "f2 pv2 bb" }, "Temperature Unit Converter"),
     div({ className: "flex" }, [
-      unitSection(dispatch, model.leftUnit, model.leftValue),
-      unitSection(dispatch, model.rightUnit, model.rightValue)
+      unitSection(
+        dispatch,
+        model.leftUnit,
+        model.leftValue,
+        leftValueInputMsg,
+        leftUnitChangedMsg
+      ),
+      unitSection(
+        dispatch,
+        model.rightUnit,
+        model.rightValue,
+        rightValueInputMsg,
+        rightUnitChangedMsg
+      )
     ]),
     pre(JSON.stringify(model, null, 2))
   ]);
