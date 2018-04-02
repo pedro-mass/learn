9011420d22969cd4844c6edb79dc0f6fd01bb74e
Pedro Mass <pedro.j.mass@gmail.com>
2018-04-01 21:39:02 -0400

    Implement view based on my own choices.

---
+++ functional-programming/udemy-functional-programming-for-beginners-with-javascript/fpjs/tempunitconvert/PLAN.md
@@ -0,0 +1,29 @@
+# Calorie Counting App Plan / Notes
+
+## Data Model
+
+Example Model/Shape:
+
+```javascript
+const initModel = {
+  leftValue: 0,
+  leftUnit: "Celsius",
+  rightValue: 32,
+  rightUnit: "Fahrenheit",
+  sourceLeft: true
+};
+```
+
+## View Functions
+
+* view
+  * formView
+    * unitView
+      * input
+      * unitSelect
+    * =
+    * unitView
+      * input
+      * unitSelect
+
+## Update / Interactions
--- functional-programming/udemy-functional-programming-for-beginners-with-javascript/fpjs/tempunitconvert/src/View.js
+++ functional-programming/udemy-functional-programming-for-beginners-with-javascript/fpjs/tempunitconvert/src/View.js
@@ -1,18 +1,44 @@
-import * as R from 'ramda';
-import hh from 'hyperscript-helpers';
-import { h } from 'virtual-dom';
+import * as R from "ramda";
+import hh from "hyperscript-helpers";
+import { h } from "virtual-dom";
 
-const {
-  div,
-  h1,
-  pre,
-} = hh(h);
+const { div, h1, pre, p, input, select, option } = hh(h);
 
+const comparisonView = div(
+  { className: "fl w-third pa0 center tc" },
+  p({ className: "pa0" }, "=")
+);
+
+function unitSection(dispatch, className, thing) {
+  const { value, unit } = thing;
+  const unitOptions = ["Celsius", "Fahrenheit"];
+  const options = unitOptions.map(unitOption =>
+    option({ selected: unitOption === unit }, unitOption)
+  );
+
+  return div({ className }, [
+    input({
+      className: "pa2 input-reset ba w-100 mb2",
+      type: "number",
+      value
+    }),
+    select({ className: "w-100" }, [options])
+  ]);
+}
 
 function view(dispatch, model) {
-  return div({ className: 'mw6 center' }, [
-    h1({ className: 'f2 pv2 bb' }, 'Temperature Unit Converter'),
-    pre(JSON.stringify(model, null, 2)),
+  return div({ className: "mw6 center" }, [
+    h1({ className: "f2 pv2 bb" }, "Temperature Unit Converter"),
+    unitSection(dispatch, "fl w-third", {
+      value: model.leftValue,
+      unit: model.leftUnit
+    }),
+    comparisonView,
+    unitSection(dispatch, "fl w-third", {
+      value: model.rightValue,
+      unit: model.rightUnit
+    }),
+    pre({ className: "fl w-100" }, JSON.stringify(model, null, 2))
   ]);
 }
 
