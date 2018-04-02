6531c186620ec3a82cc7cdc9923bfe39c86b7bb1
Pedro Mass <pedro.j.mass@gmail.com>
2018-04-02 09:41:04 -0400

    Handle unit conversions.

--- functional-programming/udemy-functional-programming-for-beginners-with-javascript/fpjs/tempunitconvert/src/Update.js
+++ functional-programming/udemy-functional-programming-for-beginners-with-javascript/fpjs/tempunitconvert/src/Update.js
@@ -36,26 +36,89 @@ export function rightUnitChangedMsg(rightUnit) {
 }
 
 const toInt = R.pipe(parseInt, R.defaultTo(0));
+const FtoC = f => 5 / 9 * (f - 32);
+const CtoF = c => 9 / 5 * (c + 32);
+const KtoC = k => k - 273.15;
+const CtoK = c => c + 273.15;
+const KtoF = k => R.pipe(KtoC, CtoF)(k);
+const FtoK = f => R.pipe(FtoC, CtoK)(f);
+const identity = x => x;
+
+const CONVERSIONS = {
+  Fahrenheit: {
+    Celsius: FtoC,
+    Kelvin: FtoK,
+    Fahrenheit: identity
+  },
+  Celsius: {
+    Fahrenheit: CtoF,
+    Kelvin: CtoK,
+    Celsius: identity
+  },
+  Kelvin: {
+    Fahrenheit: KtoF,
+    Celsius: KtoC,
+    Kelvin: identity
+  }
+};
 
 function update(msg, model) {
   switch (msg.type) {
     case MSGS.LEFT_VALUE_INPUT:
-      if (msg.leftValue === "")
-        return { ...model, sourceLeft: true, leftValue: "", rightValue: "" };
-      const leftValue = toInt(msg.leftValue);
-      return { ...model, sourceLeft: true, leftValue };
+      return updateLeftValue(msg, model);
     case MSGS.RIGHT_VALUE_INPUT:
-      if (msg.rightValue === "")
-        return { ...model, sourceLeft: true, leftValue: "", rightValue: "" };
-      const rightValue = toInt(msg.rightValue);
-      return { ...model, sourceLeft: false, rightValue };
+      return updateRightValue(msg, model);
     case MSGS.LEFT_UNIT_CHANGED:
-      const { leftUnit } = msg;
-      return { ...model, leftUnit, sourceLeft: true };
+      return updateLeftUnit(msg, model);
     case MSGS.RIGHT_UNIT_CHANGED:
-      const { rightUnit } = msg;
-      return { ...model, rightUnit, sourceLeft: false };
+      return updateRightUnit(msg, model);
   }
+  return model;
+}
+
+function updateLeftValue(msg, model) {
+  if (msg.leftValue === "")
+    return { ...model, sourceLeft: true, leftValue: "", rightValue: "" };
+  const leftValue = toInt(msg.leftValue);
+  return updateModel({ ...model, sourceLeft: true, leftValue });
+}
+
+function updateRightValue(msg, model) {
+  if (msg.rightValue === "")
+    return { ...model, sourceLeft: true, leftValue: "", rightValue: "" };
+  const rightValue = toInt(msg.rightValue);
+  return updateModel({ ...model, sourceLeft: false, rightValue });
+}
+
+function updateLeftUnit(msg, model) {
+  const { leftUnit } = msg;
+  return updateModel({ ...model, leftUnit });
+}
+
+function updateRightUnit(msg, model) {
+  const { rightUnit } = msg;
+  return updateModel({ ...model, rightUnit });
+}
+
+function updateModel(model) {
+  if (model.sourceLeft) {
+    const baseUnit = model.leftUnit;
+    const baseValue = model.leftValue;
+    const toUnit = model.rightUnit;
+
+    const rightValue = CONVERSIONS[baseUnit][toUnit](baseValue);
+
+    return { ...model, rightValue };
+  } else {
+    const baseUnit = model.rightUnit;
+    const baseValue = model.rightValue;
+    const toUnit = model.leftUnit;
+
+    const leftValue = CONVERSIONS[baseUnit][toUnit](baseValue);
+
+    return { ...model, leftValue };
+  }
+
   return model;
 }
 
