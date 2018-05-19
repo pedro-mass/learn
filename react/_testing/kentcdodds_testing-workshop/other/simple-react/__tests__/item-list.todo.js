c868b410244a8aede6298aeeb16ee710bb441558
Pedro Mass <pedro.j.mass@gmail.com>
2018-05-18 20:33:02 -0400

    Add tests for item-list component

--- react/_testing/kentcdodds_testing-workshop/other/simple-react/__tests__/item-list.todo.js
+++ react/_testing/kentcdodds_testing-workshop/other/simple-react/__tests__/item-list.todo.js
@@ -8,23 +8,50 @@
 // to testing react components.
 
 // So you can use JSX (which transpiles down to React.createElement):
-// import React from 'react'
+import React from 'react'
 //
 // So you can render the component for testing:
-// import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom'
 //
 // So you can create a react element for the component you're testing:
-// import ItemList from '../item-list'
+import ItemList from '../item-list'
+
+function createContainer(type = 'div') {
+  return document.createElement(type)
+}
+
+function render(JSX, containerToUse) {
+  const container = containerToUse || createContainer()
+  ReactDOM.render(JSX, container)
+  return container
+}
+
+function renderList(props) {
+  return render(<ItemList {...props} />)
+}
 
 // and here's an outline example of your first test:
-//   Create a "container" to render your component into (tip: use document.createElement('div'))
-//
-//   Render your component (tip: use ReactDOM.render(JSX, container))
-//
-//   Make your assertion(s) on the textContent of the container
-//   (tip: expect's toMatch function might be what you want
-//   for example: `expect('some text content').toMatch('text')`)
-//
+test('renders "no items" when no items are given', () => {
+  //   Create a "container" to render your component into (tip: use document.createElement('div'))
+  // const container = createContainer()
+  //
+  //   Render your component (tip: use ReactDOM.render(JSX, container))
+  const container = renderList({items: []})
+  //
+  //   Make your assertion(s) on the textContent of the container
+  //   (tip: expect's toMatch function might be what you want
+  //   for example: `expect('some text content').toMatch('text')`)
+  //
+  // expect(container.innerHTML).toMatch('no items')
+  expect(container.textContent).toMatch('no items')
+})
+
+test('renders items given', () => {
+  const items = ['some', 'thing', 'cool']
+  const container = renderList({items})
+  items.forEach(item => expect(container.innerHTML).toMatch(item))
+})
+
 // For your second test, it will be very similar to the first.
 
 //////// Elaboration & Feedback /////////
@@ -36,8 +63,8 @@
 /*
 http://ws.kcd.im/?ws=Testing&e=basic%20react%20test&em=
 */
-test.skip('I submitted my elaboration and feedback', () => {
-  const submitted = false // change this when you've submitted!
+test('I submitted my elaboration and feedback', () => {
+  const submitted = true // change this when you've submitted!
   expect(submitted).toBe(true)
 })
 ////////////////////////////////
