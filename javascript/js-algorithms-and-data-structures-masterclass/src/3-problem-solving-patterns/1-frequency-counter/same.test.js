32096b134ecec4d8dd830f7c73de13f9f8669ccf
Pedro Mass <pedro.j.mass@gmail.com>
2018-09-12 06:33:46 -0400

    Rename frequency counter folder

--- javascript/js-algorithms-and-data-structures-masterclass/src/3-problem-solving-patterns/1-frequency-counter/same.test.js
+++ javascript/js-algorithms-and-data-structures-masterclass/src/3-problem-solving-patterns/1-frequency-counter/same.test.js
@@ -1,4 +1,4 @@
-import { getTestCases } from '../../test-helpers'
+import { loopTestCases } from '../../test-helpers'
 import isSame from './same'
 
 function getRandomInt (max) {
@@ -9,55 +9,51 @@ function shuffle (array) {
   return array.sort(() => getRandomInt(2))
 }
 
-const cases = [
+const testCases = [
   {
     input: [[0], [0]],
-    output: true
+    output: true,
   },
   {
     input: [[0], [1]],
-    output: false
+    output: false,
   },
   {
     input: [[2], [4]],
-    output: true
+    output: true,
   },
   {
     input: [[2, 3, 5], [4, 9, 25]],
-    output: true
+    output: true,
   },
   {
     input: [[2, 3, 5], [4, 9, 25].reverse()],
-    output: true
+    output: true,
   },
   {
     input: [[2, 3, 5], shuffle([4, 9, 25])],
-    output: true
+    output: true,
   },
   {
     input: [[2, 4, 5], [4, 25]],
-    output: false
+    output: false,
   },
   {
     input: [[2, 4, 5], [4, 9, 25]],
-    output: false
+    output: false,
   },
   {
     input: [[], []],
-    output: true
+    output: true,
   },
   {
     input: [[1, 2, 2], [1, 4, 5]],
-    output: false
+    output: false,
   },
   {
     input: [[1, 2, 2], [1, 4]],
-    output: false
-  }
+    output: false,
+  },
 ]
 
-describe.each(getTestCases(cases))('isSame()', ({ input, output }) => {
-  it(`input: ${input} \t| output: ${output}`, () => {
-    expect(isSame(...input)).toEqual(output)
-  })
-})
+loopTestCases({ fnUnderTest: isSame, testCases })
