49243bb1d5f7d4cafc8eff994858591d54dbec14
Pedro Mass <pedro.j.mass@gmail.com>
2018-09-18 08:56:04 -0400

    Add a way to skip tests

--- javascript/js-algorithms-and-data-structures-masterclass/src/test-helpers.js
+++ javascript/js-algorithms-and-data-structures-masterclass/src/test-helpers.js
@@ -5,10 +5,13 @@ export function timeCallInMs (fn) {
   return t2 - t1
 }
 
-export function getTestCases (cases) {
+export function getTestCases (cases = []) {
   const filteredCases = cases.filter(c => c.only)
+  const removeSkipped = _cases => _cases.filter(c => c.skip !== true)
 
-  return filteredCases && filteredCases.length > 0 ? filteredCases : cases
+  return filteredCases && filteredCases.length > 0
+    ? removeSkipped(filteredCases)
+    : removeSkipped(cases)
 }
 
 export function loopTestCases ({ testCases, fnUnderTest }) {
