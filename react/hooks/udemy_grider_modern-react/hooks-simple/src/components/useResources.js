332f4a1beed34f3dbd6a61af31d8239ac7e0ffb2
Pedro Mass <pedro.j.mass@gmail.com>
2019-01-29 09:00:18 -0500

    refactor: Move into subfolder: hooks-simple

--- react/hooks/udemy_grider_modern-react/hooks-simple/src/components/useResources.js
+++ react/hooks/udemy_grider_modern-react/hooks-simple/src/components/useResources.js
@@ -1,11 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-const urls = {
-  posts: "https://jsonplaceholder.typicode.com/posts",
-  todos: "https://jsonplaceholder.typicode.com/todos"
-};
-
 function useResources(resource) {
   const [resources, setResources] = useState([]);
 
