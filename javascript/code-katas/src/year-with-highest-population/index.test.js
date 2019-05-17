ab2aca8fa8d53cac3232f8d02197e1cf1de0af82
Pedro Mass <pedro.j.mass@gmail.com>
2019-05-17 09:38:21 -0400

    code kata(Peak Population): Expand tests
    Ensure it works with a big dataset across all implementations

--- javascript/code-katas/src/year-with-highest-population/index.test.js
+++ javascript/code-katas/src/year-with-highest-population/index.test.js
@@ -21,4 +21,27 @@ describe.only('findMostPopulatedYear()', () => {
   Object.values(testFns).forEach(fnUnderTest =>
     loopTestCases({ fnUnderTest, testCases })
   )
+
+  it('should return the same value across all functions', () => {
+    const people = generatePeople(100)
+
+    const values = Object.values(testFns).map(fn => fn(people))
+
+    expect(values.every(v => v === values[0])).toBe(true)
+  })
 })
+
+function generatePeople (numPeople = 5) {
+  const people = []
+  while (numPeople > 0) {
+    numPeople--
+    const birthYear = randomYear()
+    const deathYear = randomYear(birthYear)
+    people.push([birthYear, deathYear])
+  }
+  return people
+}
+
+function randomYear (min = 0, max = 3000) {
+  return Math.floor(Math.random() * max + min)
+}
