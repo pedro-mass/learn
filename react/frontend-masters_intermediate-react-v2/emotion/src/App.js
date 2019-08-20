654ece1da9bedaba9b004807336be03fc9f77665
Pedro Mass <pedro.j.mass@gmail.com>
2019-08-19 22:25:13 -0400

    finished emotion module

---
+++ react/frontend-masters_intermediate-react-v2/emotion/src/App.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react"
+import ReactDOM from "react-dom"
+import { Router } from "@reach/router"
+import Details from "./Details"
+import SearchParams from "./SearchParams"
+import ThemeContext from "./ThemeContext"
+import NavBar from "./NavBar"
+
+const App = () => {
+  const theme = useState("darkblue")
+  return (
+    <ThemeContext.Provider value={theme}>
+      <div>
+        {/* <header>
+          <Link to="/">Adopt Me!</Link>
+        </header> */}
+        <NavBar />
+        <Router>
+          <SearchParams path="/" />
+          <Details path="/details/:id" />
+        </Router>
+      </div>
+    </ThemeContext.Provider>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById("root"))
---
+++ react/frontend-masters_intermediate-react-v2/emotion/src/colors.js
@@ -0,0 +1,6 @@
+export default {
+  primary: "#b43334",
+  secondary: "#d9c148",
+  dark: "#122622",
+  light: "#81a69b",
+}
