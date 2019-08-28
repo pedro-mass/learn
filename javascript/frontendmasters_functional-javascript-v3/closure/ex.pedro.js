73ce3b0927de069e1c8bdecf6a60fba54237f4ac
Pedro Mass <pedro.j.mass@gmail.com>
2019-08-28 10:37:03 -0400

    functional-javascript-v3: closure

---
+++ javascript/frontendmasters_functional-javascript-v3/closure/ex.pedro.js
@@ -0,0 +1,24 @@
+"use strict";
+
+function strBuilder(str) {
+  return value => {
+    if (typeof value !== "string") {
+      return str;
+    }
+
+    return strBuilder(str + value);
+  };
+}
+
+var hello = strBuilder("Hello, ");
+var kyle = hello("Kyle");
+var susan = hello("Susan");
+var question = kyle("?")();
+var greeting = susan("!")();
+
+console.log(strBuilder("Hello, ")("")("Kyle")(".")("")() === "Hello, Kyle.");
+console.log(hello() === "Hello, ");
+console.log(kyle() === "Hello, Kyle");
+console.log(susan() === "Hello, Susan");
+console.log(question === "Hello, Kyle?");
+console.log(greeting === "Hello, Susan!");
