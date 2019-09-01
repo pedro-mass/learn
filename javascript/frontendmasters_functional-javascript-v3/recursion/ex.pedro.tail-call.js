2aeaa5a54dd3fe712d3917c9666b4af259275621
Pedro Mass <pedro.j.mass@gmail.com>
2019-09-01 10:17:28 -0400

    functional-javascript-v3: trampoline recursion

---
+++ javascript/frontendmasters_functional-javascript-v3/recursion/ex.pedro.tail-call.js
@@ -0,0 +1,22 @@
+"use strict";
+
+function isPalindrome(str, thing = true) {
+  if (!thing) return false;
+  if (!str || str.length < 2) return true;
+
+  const first = str[0];
+  const last = str[str.length - 1];
+  return isPalindrome(str.slice(1, str.length - 1), first === last);
+}
+
+console.log(isPalindrome("") === true);
+console.log(isPalindrome("a") === true);
+console.log(isPalindrome("aa") === true);
+console.log(isPalindrome("aba") === true);
+console.log(isPalindrome("abba") === true);
+console.log(isPalindrome("abccba") === true);
+
+console.log(isPalindrome("ab") === false);
+console.log(isPalindrome("abc") === false);
+console.log(isPalindrome("abca") === false);
+console.log(isPalindrome("abcdba") === false);
