9a2578680935a0f528ef572fdb84469d9e26700e
Pedro Mass <pedro.j.mass@gmail.com>
2019-10-06 12:47:40 -0400

    mobx-state-tree tutorial: finish
    Not impressed by the writing.
    Tried improving it through PRs, but seems to not be continuous in it's code examples

--- javascript/mobx/mobx-state-tree_getting-started-tutorial/src/index.js
+++ javascript/mobx/mobx-state-tree_getting-started-tutorial/src/index.js
@@ -1,12 +1,140 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import { render } from 'react-dom'
+import { types } from 'mobx-state-tree'
+import { observer } from 'mobx-react'
+import { values } from 'mobx'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const randomId = () => Math.floor(Math.random() * 1000).toString(36)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const Todo = types
+  .model({
+    name: types.optional(types.string, ''),
+    done: types.optional(types.boolean, false),
+    user: types.maybe(types.reference(types.late(() => User))),
+  })
+  .actions(self => {
+    function setName(newName) {
+      self.name = newName
+    }
+    function setUser(user) {
+      if (user === '') {
+        // When selected value is empty, set as undefined
+        self.user = undefined
+      } else {
+        self.user = user
+      }
+    }
+    function toggle() {
+      self.done = !self.done
+    }
+
+    return { setName, setUser, toggle }
+  })
+
+const User = types.model({
+  id: types.identifier,
+  name: types.optional(types.string, ''),
+})
+
+const RootStore = types
+  .model({
+    users: types.map(User),
+    todos: types.map(Todo),
+  })
+  .views(self => ({
+    get pendingCount() {
+      return values(self.todos).filter(todo => !todo.done).length
+    },
+    get completedCount() {
+      return values(self.todos).filter(todo => todo.done).length
+    },
+    getTodosWhereDoneIs(done) {
+      return values(self.todos).filter(todo => todo.done === done)
+    },
+  }))
+  .actions(self => {
+    function addTodo(id, name) {
+      self.todos.set(id, Todo.create({ id, name }))
+    }
+
+    return { addTodo }
+  })
+
+const store = RootStore.create({
+  users: {
+    '1': {
+      id: '1',
+      name: 'mweststrate',
+    },
+    '2': {
+      id: '2',
+      name: 'mattiamanzati',
+    },
+    '3': {
+      id: '3',
+      name: 'johndoe',
+    },
+  },
+  todos: {
+    '1': {
+      name: 'Eat a cake',
+      done: true,
+    },
+  },
+})
+
+const UserPickerView = observer(props => (
+  <select
+    value={props.user ? props.user.id : ''}
+    onChange={e => props.onChange(e.target.value)}
+  >
+    <option value="">-none-</option>
+    {values(props.store.users).map(user => (
+      <option value={user.id}>{user.name}</option>
+    ))}
+  </select>
+))
+UserPickerView.displayName = 'UserPickerView'
+
+const TodoView = observer(props => (
+  <div>
+    <input
+      type="checkbox"
+      checked={props.todo.done}
+      onChange={e => props.todo.toggle()}
+    />
+    <input
+      type="text"
+      value={props.todo.name}
+      onChange={e => props.todo.setName(e.target.value)}
+    />
+    <UserPickerView
+      user={props.todo.user}
+      store={props.store}
+      onChange={userId => props.todo.setUser(userId)}
+    />
+  </div>
+))
+TodoView.displayName = 'TodoView'
+
+const TodoCounterView = observer(props => (
+  <div>
+    {props.store.pendingCount} pending, {props.store.completedCount} completed
+  </div>
+))
+TodoCounterView.displayName = 'TodoCounterView'
+
+const AppView = observer(props => (
+  <div>
+    <button onClick={e => props.store.addTodo(randomId(), 'New Task')}>
+      Add Task
+    </button>
+    {values(props.store.todos).map(todo => (
+      <TodoView store={props.store} todo={todo} />
+    ))}
+    <TodoCounterView store={props.store} />
+  </div>
+))
+AppView.displayName = 'AppView'
+
+render(<AppView store={store} />, document.getElementById('root'))
