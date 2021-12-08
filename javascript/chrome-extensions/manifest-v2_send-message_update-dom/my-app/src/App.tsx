ea48d326b4c14823820cc226407db38df6924708
Pedro Mass <pedro.j.mass@gmail.com>
2021-12-08 18:14:08 -0500

    display current tab's url

--- javascript/chrome-extensions/manifest-v2_send-message_update-dom/my-app/src/App.tsx
+++ javascript/chrome-extensions/manifest-v2_send-message_update-dom/my-app/src/App.tsx
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
+  const [url, setUrl] = useState<string>('')
+
+  /**
+   * Get current URL
+   */
+  useEffect(() => {
+    const queryInfo = { active: true, lastFocusedWindow: true }
+
+    chrome.tabs &&
+      chrome.tabs.query(queryInfo, tabs => {
+        const url = tabs[0].url ?? ''
+        setUrl(url)
+      })
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>URL:</p>
+        <p>{url}</p>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
