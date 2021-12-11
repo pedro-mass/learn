bbb39e1b3ff0a9fbf9601848ffb7eff1ac75e52b
Pedro Mass <pedro.j.mass@gmail.com>
2021-12-11 14:17:28 -0500

    finished another chrome extension tutorial

---
+++ javascript/chrome-extensions/manifest-v2_background-color/extension/src/main.js
@@ -0,0 +1,19 @@
+/*global chrome*/
+
+export async function changeColor() {
+  chrome.storage.sync.set({ color: '#3aa757' }, () => {})
+
+  await chrome.tabs.query({ active: true, currentWindow: true }, r => {
+    chrome.tabs.executeScript(
+      r[0].id,
+      { file: 'scripts/changeBgColor.js' },
+      function () {
+        if (chrome.runtime.lastError) {
+          console.error(
+            'Script injection failed: ' + chrome.runtime.lastError.message
+          )
+        }
+      }
+    )
+  })
+}
---
+++ javascript/chrome-extensions/manifest-v2_background-color/readme.md
@@ -0,0 +1,3 @@
+source: https://javascript.plainenglish.io/creating-a-chrome-extension-with-react-d92db20550cb
+
+original repo: https://github.com/onlyoneaman/chrome-extension-react
