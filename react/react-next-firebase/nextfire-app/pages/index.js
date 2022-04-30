317d5e78be8e2d33d4a48465a155757fc37d13de
Pedro Mass <pedro.j.mass@gmail.com>
2022-04-29 18:48:28 -0600

    Added a toaster

--- react/react-next-firebase/nextfire-app/pages/index.js
+++ react/react-next-firebase/nextfire-app/pages/index.js
@@ -1,13 +1,9 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-
-import Loader from '../components/Loader'
+import toast from 'react-hot-toast'
 
 export default function Home() {
   return (
     <div>
-      <Loader show />
+      <button onClick={() => toast.success('hello toast!')}>Toast Me</button>
     </div>
   )
 }
