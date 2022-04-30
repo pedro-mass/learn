5ef356ff7a99174b222425e3603580e2aa5e5320
Pedro Mass <pedro.j.mass@gmail.com>
2022-04-29 18:44:55 -0600

    Create NavBar

---
+++ react/react-next-firebase/nextfire-app/components/Navbar.tsx
@@ -0,0 +1,45 @@
+import Link from 'next/link'
+import Image from 'next/image'
+
+// Top navbar
+export default function Navbar() {
+  const user = null
+  const username = null
+
+  return (
+    <nav className="navbar">
+      <ul>
+        <li>
+          <Link href="/" passHref>
+            <button className="btn-logo">FEED</button>
+          </Link>
+        </li>
+
+        {/* user is signed-in and has username */}
+        {username && (
+          <>
+            <li className="push-left">
+              <Link href="/admin" passHref>
+                <button className="btn-blue">Write Posts</button>
+              </Link>
+            </li>
+            <li>
+              <Link href={`/${username}`} passHref>
+                <Image src={user?.photoURL} alt="user photo" />
+              </Link>
+            </li>
+          </>
+        )}
+
+        {/* user is not signed OR has not created username */}
+        {!username && (
+          <li>
+            <Link href="/enter" passHref>
+              <button className="btn-blue">Log in</button>
+            </Link>
+          </li>
+        )}
+      </ul>
+    </nav>
+  )
+}
