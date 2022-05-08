e9061994896a6c02f2e857cbdae7a96d300a7197
Pedro Mass <pedro.j.mass@gmail.com>
2022-05-07 21:03:45 -0600

    complete:  SSR User Profile Page

--- react/react-next-firebase/nextfire-app/lib/firebase.ts
+++ react/react-next-firebase/nextfire-app/lib/firebase.ts
@@ -21,4 +21,30 @@ export const auth = firebase.auth()
 export const googleAuthProvider = new firebase.auth.GoogleAuthProvider()
 
 export const firestore = firebase.firestore()
+
+/**`
+ * Gets a users/{uid} document with username
+ * @param  {string} username
+ */
+export async function getUserWithUsername(username) {
+  const usersRef = firestore.collection('users')
+  const query = usersRef.where('username', '==', username).limit(1)
+  const userDoc = (await query.get()).docs[0]
+  return userDoc
+}
+
+/**`
+ * Converts a firestore document to JSON
+ * @param  {DocumentSnapshot} doc
+ */
+export function postToJSON(doc) {
+  const data = doc.data()
+  return {
+    ...data,
+    // Gotcha! firestore timestamp NOT serializable to JSON. Must convert to milliseconds
+    createdAt: data.createdAt.toMillis(),
+    updatedAt: data.updatedAt.toMillis(),
+  }
+}
+
 export const storage = firebase.storage()
--- react/react-next-firebase/nextfire-app/pages/[username]/index.js
+++ react/react-next-firebase/nextfire-app/pages/[username]/index.js
@@ -1,7 +1,36 @@
-export default function UserPage({}) {
+import { getUserWithUsername, postToJSON } from '../../lib/firebase'
+import UserProfile from '../../components/UserProfile'
+import PostFeed from '../../components/PostFeed'
+
+export async function getServerSideProps({ query }) {
+  const { username } = query
+
+  const userDoc = await getUserWithUsername(username)
+
+  // JSON serializable data
+  let user = null
+  let posts = null
+
+  if (userDoc) {
+    user = userDoc.data()
+    const postsQuery = userDoc.ref
+      .collection('posts')
+      .where('published', '==', true)
+      .orderBy('createdAt', 'desc')
+      .limit(5)
+    posts = (await postsQuery.get()).docs.map(postToJSON)
+  }
+
+  return {
+    props: { user, posts }, // will be passed to the page component as props
+  }
+}
+
+export default function UserProfilePage({ user, posts }) {
   return (
     <main>
-      <h1>User Profile</h1>
+      <UserProfile user={user} />
+      <PostFeed posts={posts} />
     </main>
   )
 }
