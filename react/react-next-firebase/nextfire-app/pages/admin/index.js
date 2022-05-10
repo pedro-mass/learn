91e9b9fa59067b7672d64b57689ed9f8b2287398
Pedro Mass <pedro.j.mass@gmail.com>
2022-05-10 09:57:18 -0700

    completed:  Admin Pages

--- react/react-next-firebase/nextfire-app/pages/admin/index.js
+++ react/react-next-firebase/nextfire-app/pages/admin/index.js
@@ -1,7 +1,9 @@
-export default function AdminPostsPage() {
+import AuthCheck from '../../components/AuthCheck'
+
+export default function AdminPostsPage(props) {
   return (
     <main>
-      <h1>Admin Posts list</h1>
+      <AuthCheck></AuthCheck>
     </main>
   )
 }
