b6c8a30f5db46ad76041922d1ae08562360d5310
Pedro Mass <pedro.j.mass@gmail.com>
2022-05-10 09:43:30 -0700

    completed:  ISR Incremental Static Regeneration

--- react/react-next-firebase/nextfire-app/pages/[username]/[slug].js
+++ react/react-next-firebase/nextfire-app/pages/[username]/[slug].js
@@ -1,7 +1,49 @@
-export default function Page() {
-  return (
-    <main>
-      <h1>User Slug</h1>
-    </main>
-  )
+import styles from '../../styles/Post.module.css'
+import PostContent from '../../components/PostContent'
+import { firestore, getUserWithUsername, postToJSON } from '../../lib/firebase'
+import { useDocumentData } from 'react-firebase-hooks/firestore'
+
+export async function getStaticProps({ params }) {
+  const { username, slug } = params
+  const userDoc = await getUserWithUsername(username)
+
+  let post
+  let path
+
+  if (userDoc) {
+    const postRef = userDoc.ref.collection('posts').doc(slug)
+    post = postToJSON(await postRef.get())
+
+    path = postRef.path
+  }
+
+  return {
+    props: { post, path },
+    revalidate: 5000,
+  }
+}
+
+export async function getStaticPaths() {
+  // Improve by using Admin SDK to select empty docs
+  const snapshot = await firestore.collectionGroup('posts').get()
+
+  const paths = snapshot.docs.map(doc => {
+    const { slug, username } = doc.data()
+    return {
+      params: { username, slug },
+    }
+  })
+
+  return {
+    // must be in this format:
+    // paths: [
+    //   { params: { username, slug }}
+    // ],
+    paths,
+    fallback: 'blocking', // tells next to fallback to regular SSR, then it can be cached like normal ISR
+  }
+}
+
+export default function Post(props) {
+  return <main className={styles.container}></main>
 }
