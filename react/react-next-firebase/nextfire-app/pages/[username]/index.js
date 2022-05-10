c1e02f87d36ac98f4084088455b098f4447b2d3c
Pedro Mass <pedro.j.mass@gmail.com>
2022-05-10 09:50:01 -0700

    completed:  Custom 404 Page

--- react/react-next-firebase/nextfire-app/pages/[username]/index.js
+++ react/react-next-firebase/nextfire-app/pages/[username]/index.js
@@ -4,8 +4,14 @@ import PostFeed from '../../components/PostFeed'
 
 export async function getServerSideProps({ query }) {
   const { username } = query
-
   const userDoc = await getUserWithUsername(username)
+
+  // If no user, short circuit to 404 page
+  if (!userDoc) {
+    return {
+      notFound: true,
+    }
+  }
 
   // JSON serializable data
   let user = null
