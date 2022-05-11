91761a5c4e80c22d4a572527d1a0f89300bb4bf0
Pedro Mass <pedro.j.mass@gmail.com>
2022-05-10 20:17:52 -0700

    Post Editing Form

--- react/react-next-firebase/nextfire-app/pages/admin/[slug].js
+++ react/react-next-firebase/nextfire-app/pages/admin/[slug].js
@@ -1,10 +1,111 @@
-import Metatags from '../../components/Metatags'
+import styles from '../../styles/Admin.module.css'
+import AuthCheck from '../../components/AuthCheck'
+import { firestore, auth, serverTimestamp } from '../../lib/firebase'
 
-export default function AdminPostEdit({}) {
+import { useState } from 'react'
+import { useRouter } from 'next/router'
+
+import { useDocumentData } from 'react-firebase-hooks/firestore'
+import { useForm } from 'react-hook-form'
+import ReactMarkdown from 'react-markdown'
+import Link from 'next/link'
+import toast from 'react-hot-toast'
+
+export default function AdminPostEdit(props) {
   return (
-    <main>
-      <Metatags title="admin page" />
-      <h1>Admin Post Edit</h1>
+    <AuthCheck>
+      <PostManager />
+    </AuthCheck>
+  )
+}
+
+function PostManager() {
+  const [preview, setPreview] = useState(false)
+
+  const router = useRouter()
+  const { slug } = router.query
+
+  const postRef = firestore
+    .collection('users')
+    .doc(auth.currentUser.uid)
+    .collection('posts')
+    .doc(slug)
+  // can switch to useDocumentDataOnce(), fetches it only once instead of keeping this with live data
+  const [post] = useDocumentData(postRef)
+
+  return (
+    <main className={styles.container}>
+      {post && (
+        <>
+          <section>
+            <h1>{post.title}</h1>
+            <p>ID: {post.slug}</p>
+
+            <PostForm
+              postRef={postRef}
+              defaultValues={post}
+              preview={preview}
+            />
+          </section>
+
+          <aside>
+            <h3>Tools</h3>
+            <button onClick={() => setPreview(!preview)}>
+              {preview ? 'Edit' : 'Preview'}
+            </button>
+            {/* eslint-disable-next-line @next/next/link-passhref */}
+            <Link href={`/${post.username}/${post.slug}`}>
+              <button className="btn-blue">Live view</button>
+            </Link>
+          </aside>
+        </>
+      )}
     </main>
+  )
+}
+
+function PostForm({ defaultValues, postRef, preview }) {
+  const { register, handleSubmit, reset, watch } = useForm({
+    defaultValues,
+    mode: 'onChange',
+  })
+
+  const updatePost = async ({ content, published }) => {
+    await postRef.update({
+      content,
+      published,
+      updatedAt: serverTimestamp(),
+    })
+
+    reset({ content, published })
+
+    toast.success('Post updated successfully!')
+  }
+
+  return (
+    <form onSubmit={handleSubmit(updatePost)}>
+      {preview && (
+        <div className="card">
+          <ReactMarkdown>{watch('content')}</ReactMarkdown>
+        </div>
+      )}
+
+      <div className={preview ? styles.hidden : styles.controls}>
+        <textarea {...register('content')}></textarea>
+
+        <fieldset>
+          <input
+            className={styles.checkbox}
+            type="checkbox"
+            {...register('published')}
+          />
+          <label>Published</label>
+        </fieldset>
+
+        <button type="submit" className="btn-green">
+          Save Changes
+        </button>
+      </div>
+    </form>
   )
 }
