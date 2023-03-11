88e149bbb6dd823d2f4161b1618a027d8d0e6a29
pedro <drom296@gmail.com>
2023-03-11 13:56:24 -0500

    finish playing around with chrome extension framework

---
+++ javascript/chrome-extensions/crxjs_chrome-getting-started-v3_3-4-23/react/src/content.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+
+const root = document.createElement('div')
+root.id = 'crx-root'
+document.body.append(root)
+
+ReactDOM.createRoot(root).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
