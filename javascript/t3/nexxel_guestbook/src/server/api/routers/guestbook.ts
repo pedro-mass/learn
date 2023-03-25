7c85cfd085b53465f5c0fe2243653622c9bfa1d2
pedro <drom296@gmail.com>
2023-03-25 13:09:39 -0400

    nexxel_guestbook

---
+++ javascript/t3/nexxel_guestbook/src/server/api/routers/guestbook.ts
@@ -0,0 +1,42 @@
+import { z } from "zod";
+import { createTRPCRouter, protectedProcedure, publicProcedure } from "../trpc";
+
+export const guestbookRouter = createTRPCRouter({
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    try {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+      return await ctx.prisma.guestbook.findMany({
+        select: {
+          name: true,
+          message: true,
+        },
+        orderBy: {
+          createdAt: "desc",
+        },
+      });
+    } catch (error) {
+      console.log("error", error);
+    }
+  }),
+  postMessage: protectedProcedure
+    .input(
+      z.object({
+        name: z.string(),
+        message: z.string(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      // ctx.prisma.guestbook.create
+      try {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+        await ctx.prisma.guestbook.create({
+          data: {
+            name: input.name,
+            message: input.message,
+          },
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    }),
+});
