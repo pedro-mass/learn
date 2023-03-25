b8c676b45ffb40d2e423da74ab7d8855186db12a
pedro <drom296@gmail.com>
2023-03-25 14:32:33 -0400

    theo-tutorial: add auth

--- javascript/t3/theo-tutorial/src/pages/index.tsx
+++ javascript/t3/theo-tutorial/src/pages/index.tsx
@@ -1,11 +1,15 @@
+import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
 import { type NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
 
 import { api } from "~/utils/api";
+import SignInPage from "./sign-in/[[...index]]";
 
 const Home: NextPage = () => {
   const hello = api.example.hello.useQuery({ text: "from tRPC" });
+
+  const user = useUser();
 
   return (
     <>
@@ -15,7 +19,12 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+        <div>
+          {!user.isSignedIn && <SignInButton />}
+          {user.isSignedIn && <SignOutButton />}
+        </div>
+        <SignInPage />
+        {/* <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
             Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
           </h1>
@@ -46,7 +55,7 @@ const Home: NextPage = () => {
           <p className="text-2xl text-white">
             {hello.data ? hello.data.greeting : "Loading tRPC query..."}
           </p>
-        </div>
+        </div> */}
       </main>
     </>
   );
