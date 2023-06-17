6dd14dada9d17b5ec0db3d777f8109e9b82034b8
pedro <drom296@gmail.com>
2023-06-17 19:22:38 -0400

    breadit: starting sign-in page

---
+++ javascript/nextjs/breadit/src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+const page: FC = () => {
+  return (
+    <div className="absolute inset-0">
+      <div className="h-full max-w-2xl mx-auto flex flex-col items-center justify-center gap-20"></div>
+    </div>
+  );
+};
+
+export default page;
