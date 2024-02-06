edcbe0c85b053cb4b08e760bc9e2d1a8b22f892d
pedro <drom296@gmail.com>
2024-02-06 01:21:27 +0000

    express: mongodb setup

--- javascript/express/coding_with_antonio/src/index.ts
+++ javascript/express/coding_with_antonio/src/index.ts
@@ -1,9 +1,11 @@
+import "dotenv/config";
 import express from "express";
 import http from "http";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import compression from "compression";
 import cors from "cors";
+import mongoose from "mongoose";
 
 const app = express();
 
@@ -21,4 +23,14 @@ const server = http.createServer(app);
 
 server.listen(8080, () => {
   console.log("Server is running on http://localhost:8080/");
+});
+
+mongoose.Promise = Promise;
+mongoose.connect(process.env.MONGO_URL);
+
+mongoose.connection.on("connected", () => {
+  console.log("Mongoose connected");
+});
+mongoose.connection.on("error", (error: Error) => {
+  console.error("Database connection error:", error);
 });
