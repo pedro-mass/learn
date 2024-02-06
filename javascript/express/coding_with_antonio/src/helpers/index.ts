6cf148f074d1f0a39d62f12915b6727bf15e4c4e
pedro <drom296@gmail.com>
2024-02-06 01:31:57 +0000

    express: auth helpers

---
+++ javascript/express/coding_with_antonio/src/helpers/index.ts
@@ -0,0 +1,9 @@
+import crypto from "crypto";
+
+export const random = () => crypto.randomBytes(128).toString("base64");
+export const authentication = (salt: string, password: string) => {
+  return crypto
+    .createHmac("sha256", [salt, password].join("/"))
+    .update(process.env.SECRET)
+    .digest("hex");
+};
