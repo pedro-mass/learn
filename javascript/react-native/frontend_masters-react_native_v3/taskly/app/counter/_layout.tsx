8e8e5a92ed1c6bef11f67ea0546137490f4b0b57
pedromass <pedro.mass@goosehead.com>
2024-09-18 09:39:00 -0400

    react native: Add a stack navigator for history

---
+++ javascript/react-native/frontend_masters-react_native_v3/taskly/app/counter/_layout.tsx
@@ -0,0 +1,31 @@
+import { MaterialIcons } from "@expo/vector-icons";
+import { Link, Stack } from "expo-router";
+import { Pressable } from "react-native";
+import { theme } from "../../theme";
+
+export default function Layout() {
+  return (
+    <Stack>
+      <Stack.Screen
+        name="index"
+        options={{
+          title: "Counter",
+          headerRight: () => {
+            return (
+              <Link href="/counter/history" asChild>
+                <Pressable hitSlop={20}>
+                  <MaterialIcons
+                    name="history"
+                    size={32}
+                    color={theme.colorGrey}
+                  />
+                </Pressable>
+              </Link>
+            );
+          },
+        }}
+      />
+      <Stack.Screen name="history" options={{ title: "History" }} />
+    </Stack>
+  );
+}
