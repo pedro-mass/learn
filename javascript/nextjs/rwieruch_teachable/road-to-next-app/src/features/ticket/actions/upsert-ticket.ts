37cc29bb04bf895fe755442b3ad0fd970ec009f2
Pedro Mass <pedro.j.mass@gmail.com>
2025-03-26 11:41:23 -0400

    road-to-next: Form Validation

--- javascript/nextjs/rwieruch_teachable/road-to-next-app/src/features/ticket/actions/upsert-ticket.ts
+++ javascript/nextjs/rwieruch_teachable/road-to-next-app/src/features/ticket/actions/upsert-ticket.ts
@@ -4,22 +4,32 @@ import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import { prisma } from "@/lib/prisma";
 import { ticketPath, ticketsPath } from "@/paths";
+import { z } from "zod";
+
+const upsertTicketSchema = z.object({
+  title: z.string().min(1).max(191),
+  content: z.string().min(1).max(1024),
+});
 
 export const upsertTicket = async (
   id: string | undefined,
   _actionState: { message: string },
   formData: FormData
 ) => {
-  const data = {
-    title: formData.get("title") as string,
-    content: formData.get("content") as string,
-  };
+  try {
+    const data = upsertTicketSchema.parse({
+      title: formData.get("title") as string,
+      content: formData.get("content") as string,
+    });
 
-  await prisma.ticket.upsert({
-    where: { id: id || "" },
-    update: data,
-    create: data,
-  });
+    await prisma.ticket.upsert({
+      where: { id: id || "" },
+      update: data,
+      create: data,
+    });
+  } catch (error) {
+    return { message: "Something went wrong" };
+  }
 
   revalidatePath(ticketsPath());
 
