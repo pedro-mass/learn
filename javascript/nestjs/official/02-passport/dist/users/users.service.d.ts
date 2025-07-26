4b022c88f1291a3909e126f1091ce71d260d0c89
Pedro Mass <pedro.j.mass@gmail.com>
2025-07-26 04:32:37 -0400

    Add .gitignore and remove dist/ directory from tracking
    - Add comprehensive .gitignore file for NestJS project
    - Remove compiled output from version control
    - dist/ directory will be ignored going forward

--- javascript/nestjs/official/02-passport/dist/users/users.service.d.ts
+++
@@ -1,9 +0,0 @@
-export type User = {
-    userId: number;
-    username: string;
-    password: string;
-};
-export declare class UsersService {
-    private readonly users;
-    findOne(username: string): Promise<User | undefined>;
-}
