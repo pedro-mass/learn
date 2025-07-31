a4b210502d6318a06bb8df4424b7aabcda4e0cd2
Pedro Mass <pedro.j.mass@gmail.com>
2025-07-30 20:13:28 -0400

    nestjs: freecodecamp: 02 JWT auth: CRUD users

--- javascript/nestjs/free-code-camp/02-jwt-auth/src/app.module.ts
+++ javascript/nestjs/free-code-camp/02-jwt-auth/src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    // MongooseModule.forRoot(
+    //   process.env.MONGO_URI ?? 'mongodb://localhost:27017/auth-workshop',
+    // ),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        uri:
+          configService.get<string>('MONGO_URI') ??
+          'mongodb://localhost:27017/auth-workshop',
+      }),
+      inject: [ConfigService],
+    }),
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ javascript/nestjs/free-code-camp/02-jwt-auth/src/users/users.service.ts
@@ -0,0 +1,36 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from './entities/user.entity';
+
+@Injectable()
+export class UsersService {
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const user = await this.userModel.create(createUserDto);
+    return user.save();
+  }
+
+  findAll() {
+    return this.userModel.find();
+  }
+
+  async findOne(id: number) {
+    const user = await this.userModel.findById(id);
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    return user;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return this.userModel.findByIdAndUpdate(id, updateUserDto, { new: true });
+  }
+
+  remove(id: number) {
+    return this.userModel.findByIdAndDelete(id);
+  }
+}
