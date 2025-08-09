5cecb607c26a45a0e5185304a3974290200195ad
Pedro Mass <pedro.j.mass@gmail.com>
2025-08-09 10:07:11 -0400

    nextjs: theo-drive-tutorial: cleanup data access pattern

--- javascript/nextjs/01-theo-drive-tutorial/src/app/f/[folderId]/page.tsx
+++ javascript/nextjs/01-theo-drive-tutorial/src/app/f/[folderId]/page.tsx
@@ -1,31 +1,10 @@
-import { db } from "~/server/db";
-import {
-  files_table as filesSchema,
-  folders_table as foldersSchema,
-} from "~/server/db/schema";
-import DriveContents from "../../drive-contents";
 import z from "zod";
-import { eq } from "drizzle-orm";
-
-async function getAllParents(folderId: number) {
-  const parents = [];
-  let currentId: number | null = folderId;
-  while (currentId !== null) {
-    const folder = await db
-      .selectDistinct()
-      .from(foldersSchema)
-      .where(eq(foldersSchema.id, currentId));
-
-    if (!folder[0]) {
-      throw new Error("Parent folder not found");
-    }
-
-    parents.unshift(folder[0]);
-    currentId = folder[0].parent;
-  }
-
-  return parents;
-}
+import {
+  getAllParentsForFolder,
+  getFiles,
+  getFolders,
+} from "~/server/db/queries";
+import DriveContents from "../../drive-contents";
 
 export default async function GoogleDriveClone(props: {
   params: Promise<{ folderId: string }>;
@@ -44,22 +23,10 @@ export default async function GoogleDriveClone(props: {
 
   const safeParams = parsedParams.data;
 
-  const filesPromise = db
-    .select()
-    .from(filesSchema)
-    .where(eq(filesSchema.parent, safeParams.folderId));
-
-  const foldersPromise = db
-    .select()
-    .from(foldersSchema)
-    .where(eq(foldersSchema.parent, safeParams.folderId));
-
-  const parentsPromise = getAllParents(safeParams.folderId);
-
   const [files, folders, parents] = await Promise.all([
-    filesPromise,
-    foldersPromise,
-    parentsPromise,
+    getFiles(safeParams.folderId),
+    getFolders(safeParams.folderId),
+    getAllParentsForFolder(safeParams.folderId),
   ]);
 
   return <DriveContents files={files} folders={folders} parents={parents} />;
---
+++ javascript/nextjs/01-theo-drive-tutorial/src/server/db/queries.ts
@@ -0,0 +1,37 @@
+import { eq } from "drizzle-orm";
+import { db } from "~/server/db";
+import {
+  files_table as filesSchema,
+  folders_table as foldersSchema,
+} from "~/server/db/schema";
+
+export async function getAllParentsForFolder(folderId: number) {
+  const parents = [];
+  let currentId: number | null = folderId;
+  while (currentId !== null) {
+    const folder = await db
+      .selectDistinct()
+      .from(foldersSchema)
+      .where(eq(foldersSchema.id, currentId));
+
+    if (!folder[0]) {
+      throw new Error("Parent folder not found");
+    }
+
+    parents.unshift(folder[0]);
+    currentId = folder[0].parent;
+  }
+
+  return parents;
+}
+
+export function getFolders(folderId: number) {
+  return db
+    .select()
+    .from(foldersSchema)
+    .where(eq(foldersSchema.parent, folderId));
+}
+
+export function getFiles(folderId: number) {
+  return db.select().from(filesSchema).where(eq(filesSchema.parent, folderId));
+}
