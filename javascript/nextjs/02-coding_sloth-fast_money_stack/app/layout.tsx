997cba8670325e82fe0ce0d8ea20912294e17091
Pedro Mass <pedro.j.mass@gmail.com>
2025-08-13 21:24:09 -0400

    nextjs: 02-coding_sloth-fast_money_stack: configure clerk to work with convex

--- javascript/nextjs/02-coding_sloth-fast_money_stack/app/layout.tsx
+++ javascript/nextjs/02-coding_sloth-fast_money_stack/app/layout.tsx
@@ -1,3 +1,12 @@
+import { ConvexClientProvider } from "@/components/ConvexClientProvider";
+import {
+  ClerkProvider,
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/nextjs";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -27,8 +36,30 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ClerkProvider>
+          <ConvexClientProvider>
+            <Header />
+            {children}
+          </ConvexClientProvider>
+        </ClerkProvider>
       </body>
     </html>
+  );
+}
+function Header() {
+  return (
+    <header className="flex justify-end items-center p-4 gap-4 h-16">
+      <SignedOut>
+        <SignInButton />
+        <SignUpButton>
+          <button className="bg-[#6c47ff] text-ceramic-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+            Sign Up
+          </button>
+        </SignUpButton>
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </header>
   );
 }
--- javascript/nextjs/02-coding_sloth-fast_money_stack/readme.md
+++ javascript/nextjs/02-coding_sloth-fast_money_stack/readme.md
@@ -17,4 +17,11 @@ Clerk billing docs: https://clerk.com/docs/billing/overview
 2. set up convex
 
 - https://docs.convex.dev/quickstart/nextjs
--
+- https://docs.convex.dev/client/react/nextjs/
+
+3. configure clerk with convex
+
+- app specific: https://dashboard.clerk.com/apps/app_31Fwbema3c4wJ8aMaIvAzSszeZN/instances/ins_31FwbZDfyAFPWCC9gAq4qJm3YjH
+- https://clerk.com/docs/integrations/databases/convex
+  - https://clerk.com/docs/references/nextjs/custom-sign-in-or-up-page?_gl=1*18vsloh*_gcl_au*MjY0MzM2NjYyLjE3NTUxMzE2MzU.*_ga*OTI2NDM2NzY3LjE3NTUxMzMxNTE.*_ga_1WMF5X234K*czE3NTUxMzMxNTAkbzEkZzEkdDE3NTUxMzM2OTMkajYwJGwwJGgw
+- https://docs.convex.dev/auth/clerk
