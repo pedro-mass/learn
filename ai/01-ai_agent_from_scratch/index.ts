c7c31b3d5fa74ac8f38608d84bd33d819ec78abf
Pedro Mass <pedro.j.mass@gmail.com>
2025-08-20 18:51:04 -0400

    ai: 01-ai_agent_from_scratch: completed step-3

--- ai/01-ai_agent_from_scratch/index.ts
+++ ai/01-ai_agent_from_scratch/index.ts
@@ -1,6 +1,6 @@
 import 'dotenv/config'
-import { runLLM } from './src/llm'
-import { addMessages, getMessages } from './src/memory'
+import { runAgent } from './src/agent'
+import { z } from 'zod'
 
 const userMessage = process.argv[2]
 
@@ -9,13 +9,14 @@ if (!userMessage) {
   process.exit(1)
 }
 
-await addMessages([{ role: 'user', content: userMessage }])
-const messages = await getMessages()
+const weatherTool = {
+  name: 'get_weather',
+  parameters: z.object({}),
+}
 
-const response = await runLLM({
-  messages,
+const response = await runAgent({
+  userMessage,
+  tools: [weatherTool],
 })
-
-await addMessages([{ role: 'assistant', content: response }])
 
 console.log(response)
--- ai/01-ai_agent_from_scratch/src/llm.ts
+++ ai/01-ai_agent_from_scratch/src/llm.ts
@@ -1,12 +1,24 @@
 import type { AIMessage } from '../types'
 import { openai } from './ai'
+import { zodFunction } from 'openai/helpers/zod'
 
-export const runLLM = async ({ messages }: { messages: AIMessage[] }) => {
+export const runLLM = async ({
+  messages,
+  tools,
+}: {
+  messages: AIMessage[]
+  tools: any[]
+}) => {
+  const formattedTools = tools.map(zodFunction)
+
   const response = await openai.chat.completions.create({
     model: 'gpt-4o-mini',
     temperature: 0.1, // how creative the model is, higher = more creative | reduces the randomness of the model
     messages,
+    tools: formattedTools,
+    tool_choice: 'auto', // let the AI figure it out
+    parallel_tool_calls: false, // easier to debug
   })
 
-  return response.choices[0].message.content
+  return response.choices[0].message
 }
